254e7a5e55513a351665bad35d91f45615fc37ce
Italo <italogabrielvc@gmail.com>
2021-11-24 17:32:07 -0300

    App orçamento pessoal - validando dados antes do registro parte 2

--- Aula 273 a 291/app1/app.js
+++ Aula 273 a 291/app1/app.js
@@ -66,12 +66,12 @@ function cadastrarDespesa() {
 	)
 
 	if(despesa.validarDados()) {
-		//bd.gravar(despesa)
+		bd.gravar(despesa)
 		//dialog de sucesso
-		console.log('Dados válidos')
+		$('#sucessoGravacao').modal('show')
 	}else {
 		//dialog de erro
-		console.log('Dados invalidos')
+		$('#erroGravacao').modal('show')
 	}
 }
 
